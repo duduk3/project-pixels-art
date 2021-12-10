0ec02e9cd93473326ce61ccba0d51a99d88ccf9a
EDUARDO FERREIRA <dupearljam@gmail.com>
2021-12-10 18:53:15 -0300

    requisito 7 -- adiciona funçao ao evento

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable no-undef */
+/* eslint-disable space-in-parens */
 /* eslint-disable sonarjs/cognitive-complexity */
 /* eslint-disable complexity */
 /* eslint-disable eqeqeq */
@@ -13,20 +16,16 @@
 /* eslint-disable prefer-const */
 /* eslint-disable quotes */
 let selectedBlack = document.querySelector(".black");
-selectedBlack.classList.add('selected');
-
 let selectedSecond = document.querySelector(".second");
-
 let selectedThird = document.querySelector(".third");
-
 let selectedFourth = document.querySelector(".fourth");
 
+selectedBlack.classList.add('selected');
 
 selectedBlack.addEventListener('click', selectColor);
 selectedSecond.addEventListener('click', selectColor);
 selectedThird.addEventListener('click', selectColor);
 selectedFourth.addEventListener('click', selectColor);
-
 
 function selectColor(event) {
     event.target.classList.add('selected');
@@ -40,8 +39,35 @@ function selectColor(event) {
     if (event.target !== selectedThird && selectedThird.classList.length > 2) {
         selectedThird.classList.remove('selected');
     }
-    if(event.target !== selectedFourth && selectedFourth.classList.length > 2) {
+    if (event.target !== selectedFourth && selectedFourth.classList.length > 2) {
         selectedFourth.classList.remove('selected');
     }
 }
+
+let selectedPixel = document.querySelectorAll(".pixel");
+console.log(selectedPixel);
+
+selectedPixel[0].addEventListener('click', selectPixel);
+
+
+function selectPixel(event) {
+    if (selectedBlack.classList.length > 2 ) {
+        event.target.className = 'pixel black';   
+        event.target.classList.add('black');   
+    }
+    if (selectedSecond.classList.length > 2 ) {
+        event.target.className = 'pixel second';   
+        event.target.classList.add('second');   
+    }
+    if (selectedThird.classList.length > 2 ) {
+        event.target.className = 'pixel third';   
+        event.target.classList.add('third');   
+    }
+    if (selectedFourth.classList.length > 2 ) {
+        event.target.className = 'pixel fourth';   
+        event.target.classList.add('fourth');   
+    }
+}
+
+
 
